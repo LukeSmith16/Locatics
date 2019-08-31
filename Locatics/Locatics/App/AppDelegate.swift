da500f0ddd58ba7d7639dd9f019c6ac03b791597
Luke Smith <luke@appoly.co.uk>
2019-08-31 20:51:37 +0100

    Added SwiftLint
    Added SwiftLint run script and configured it.

--- Locatics/Locatics/App/AppDelegate.swift
+++ Locatics/Locatics/App/AppDelegate.swift
@@ -6,14 +6,14 @@
 //  Copyright © 2019 Luke Smith. All rights reserved.
 //
 
+// swiftlint:disable line_length
+
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         return true
     }
 }
-
