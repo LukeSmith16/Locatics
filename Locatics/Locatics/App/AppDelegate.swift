a63de74bf496b17bd77e837e2dabaa63002046eb
Luke Smith <luke@appoly.co.uk>
2019-08-31 16:58:37 +0100

    Setup Xcode project
    Setup Xcode project with testing target

---
+++ Locatics/Locatics/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Locatics
+//
+//  Created by Luke Smith on 31/08/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
