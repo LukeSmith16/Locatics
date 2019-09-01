b9530513e0b664505ddf33481bb2aad368dd450f
Luke Smith <luke@appoly.co.uk>
2019-09-01 12:27:55 +0100

    Setup TravisCI
    Configured and added TravisCI, also had to add storyboard file to make tests pass.

---
+++ Locatics/LocaticsTests/App/LocaticsTest.swift
@@ -0,0 +1,12 @@
+//
+//  LocaticsTest.swift
+//  LocaticsTests
+//
+//  Created by Luke Smith on 01/09/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import XCTest
+
+@testable import Locatics
+class LocaticsTest: XCTestCase {}
