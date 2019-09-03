ddb8252bf9e8d520147b1e1deaa19ebe5e332f09
Luke Smith <luke@appoly.co.uk>
2019-09-03 18:40:39 +0100

    Added RootModuleFactory with tests

---
+++ Locatics/Locatics/App/Coordinators/Factories/RootModuleFactory.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewFactory.swift
+//  Locatics
+//
+//  Created by Luke Smith on 03/09/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import UIKit
+
+protocol RootModuleFactoryInterface {
+    func createRootNavigationController() -> UINavigationController
+}
+
+class RootModuleFactory: RootModuleFactoryInterface {
+    func createRootNavigationController() -> UINavigationController {
+        return UINavigationController()
+    }
+}
