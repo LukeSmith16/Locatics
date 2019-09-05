4ffe45fbbff3472c3907d2fa71ec64406adb3297
Luke Smith <luke@appoly.co.uk>
2019-09-05 13:46:44 +0100

    Added location permissions manager with tests

---
+++ Locatics/Locatics/Onboarding/Models/LocationPermissionsManager.swift
@@ -0,0 +1,52 @@
+//
+//  PermissionsManager.swift
+//  Locatics
+//
+//  Created by Luke Smith on 05/09/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol LocationPermissionsManagerDelegate: class {
+    func permissionsGranted()
+    func permissionsDenied()
+}
+
+protocol LocationPermissionsManagerInterface {
+    var delegate: LocationPermissionsManagerDelegate? {get set}
+
+    func hasAuthorizedLocationPermissions() -> Bool
+    func authorizeLocationPermissions()
+}
+
+class LocationPermissionsManager: NSObject, LocationPermissionsManagerInterface {
+    weak var delegate: LocationPermissionsManagerDelegate?
+
+    let locationManager = CLLocationManager()
+
+    override init() {
+        super.init()
+        locationManager.delegate = self
+    }
+
+    func hasAuthorizedLocationPermissions() -> Bool {
+        return CLLocationManager.authorizationStatus() == .authorizedAlways
+    }
+
+    func authorizeLocationPermissions() {
+        locationManager.requestAlwaysAuthorization()
+    }
+}
+
+extension LocationPermissionsManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedAlways else {
+            delegate?.permissionsDenied()
+            return
+        }
+
+        delegate?.permissionsGranted()
+    }
+}
