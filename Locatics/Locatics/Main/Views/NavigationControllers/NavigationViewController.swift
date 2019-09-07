73b8c9d58db9d537afce41597501693ba8187482
Luke Smith <lukeedwardsmith97@gmail.com>
2019-09-07 21:43:11 +0100

    Added custom NavigationViewController to setup appearances

---
+++ Locatics/Locatics/Main/Views/NavigationControllers/NavigationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LocaticsMapNavigationViewController.swift
+//  Locatics
+//
+//  Created by Luke Smith on 07/09/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.setupDefaultNavigationBarAppearance()
+    }
+}
