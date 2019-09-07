09fbc2338ba11880e7143f90a7be746772fe40f9
Luke Smith <lukeedwardsmith97@gmail.com>
2019-09-07 21:50:07 +0100

    Added extra tests for LocationManager

--- Locatics/LocaticsTests/Main/Models/Map/LocationManagerTests.swift
+++ Locatics/LocaticsTests/Main/Models/Map/LocationManagerTests.swift
@@ -52,6 +52,36 @@ class LocationManagerTests: XCTestCase {
     func test_delegate_isNotNil() {
         XCTAssertNotNil(sut.locationProvider.delegate)
     }
+
+    func test_didUpdateLocations_callsCompletionBlockWithLocationWhenValid() {
+        let givenLocation = CLLocation(latitude: 50.0, longitude: 25.0)
+
+        sut.findCurrentLocation { (result) in
+            switch result {
+            case .success(let location):
+                XCTAssertEqual(location.coordinate.latitude, givenLocation.coordinate.latitude)
+                XCTAssertEqual(location.coordinate.longitude, givenLocation.coordinate.longitude)
+            case .failure(let failure):
+                XCTFail(failure.localizedDescription)
+            }
+        }
+
+        sut.locationManager(CLLocationManager(), didUpdateLocations: [givenLocation])
+    }
+
+    func test_didUpdateLocations_callsCompletionBlockWithErrorWhenInvalid() {
+        sut.findCurrentLocation { (result) in
+            switch result {
+            case .success(let location):
+                XCTFail("Shouldn't be returning location when error occurs - \(location.coordinate)")
+            case .failure(let failure):
+                XCTAssertEqual(failure.localizedDescription,
+                               LocationError.locationNotFound.localizedDescription)
+            }
+        }
+
+        sut.locationManager(CLLocationManager(), didUpdateLocations: [])
+    }
 }
 
 private extension LocationManagerTests {
