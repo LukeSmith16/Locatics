5f9b851b6ae3eb2c77bf531986a42df2fca72195
LukeSmith16 <lukeedwardsmith97@gmail.com>
2019-09-28 21:03:43 +0100

    Added multicast delegate communicator pattern

---
+++ Locatics/App/Communicators/MulticastDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  MulticastDelegate.swift
+//  Locatics
+//
+//  Created by Luke Smith on 28/09/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+// swiftlint:disable force_cast
+
+import Foundation
+
+class MulticastDelegate<T> {
+
+    private let delegates: NSHashTable<AnyObject> = NSHashTable.weakObjects()
+
+    func add(_ delegate: T) {
+        delegates.add(delegate as AnyObject)
+    }
+
+    func remove(_ delegateToRemove: T) {
+        for delegate in delegates.allObjects.reversed() where delegate === delegateToRemove as AnyObject {
+            delegates.remove(delegate)
+        }
+    }
+
+    func invoke(_ invocation: (T) -> Void) {
+        for delegate in delegates.allObjects.reversed() {
+            invocation(delegate as! T)
+        }
+    }
+}
