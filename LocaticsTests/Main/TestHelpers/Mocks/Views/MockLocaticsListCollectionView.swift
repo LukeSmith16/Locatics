224032186a7d2d903cafb27e0287dd960fee3be5
LukeSmith16 <lukeedwardsmith97@gmail.com>
2019-10-22 15:45:23 +0100

    Create MockLocaticsListCollectionView.swift

---
+++ LocaticsTests/Main/TestHelpers/Mocks/Views/MockLocaticsListCollectionView.swift
@@ -0,0 +1,58 @@
+//
+//  MockLocaticsListCollectionView.swift
+//  LocaticsTests
+//
+//  Created by Luke Smith on 21/10/2019.
+//  Copyright © 2019 Luke Smith. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+@testable import Locatics
+class MockLocaticsListCollectionView: LocaticsListCollectionView {
+    var expectation: XCTestExpectation?
+
+    var cellGotDequeued = false
+
+    var calledPerformBatchUpdates = false
+    var insertedItems = false
+    var deletedItems = false
+    var reloadedItems = false
+
+    var passedIndexPath: IndexPath?
+
+    override func dequeueReusableCell(withReuseIdentifier identifier: String,
+                                      for indexPath: IndexPath) -> UICollectionViewCell {
+        cellGotDequeued = true
+
+        return super.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+    }
+
+    override func insertItems(at indexPaths: [IndexPath]) {
+        insertedItems = true
+        passedIndexPath = indexPaths.first
+
+        super.insertItems(at: indexPaths)
+    }
+
+    override func deleteItems(at indexPaths: [IndexPath]) {
+        deletedItems = true
+        passedIndexPath = indexPaths.first
+
+        super.deleteItems(at: indexPaths)
+    }
+
+    override func reloadItems(at indexPaths: [IndexPath]) {
+        reloadedItems = true
+        passedIndexPath = indexPaths.first
+
+        super.reloadItems(at: indexPaths)
+    }
+
+    override func performBatchUpdates(_ updates: (() -> Void)?, completion: ((Bool) -> Void)? = nil) {
+        calledPerformBatchUpdates = true
+        super.performBatchUpdates(updates, completion: completion)
+        expectation?.fulfill()
+    }
+}
